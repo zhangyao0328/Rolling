d1275f28f7f723feac8bbddb9132cea41794f0fd
zhangyao <zhangyao0328@gmail.com>
2018-07-29 22:21:42 +0800

    创建

---
+++ app/src/main/java/com/rollling/app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.rollling.app;
+
+import android.app.Application;
+
+/**
+ * @author zhangyao
+ * @date 2018/7/29  22:10
+ * @E-mail android_n@163.com
+ */
+public class MyApplication extends Application{
+}
